9c1ea231b71cf5cbb42e04d3eb090baa82a77d44
Mike Price <idflyfish@hotmail.com>
2019-07-15 10:25:56 -0600

    Effects

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    // Getting breeds from the api. This runs once on first render and then only when animal, setBreed, setBreeds changes.
+    // Note: setBreed and setBreeds will never change (they are functions), but the core team wanted to make sure that all
+    // dependencies external to this function are declared.
+    pet.breeds(animal).then(({ breeds: breedsFromApi }) => {
+      const breedStrings = breedsFromApi.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
